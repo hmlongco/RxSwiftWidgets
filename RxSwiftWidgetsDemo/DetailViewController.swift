5813ea983449e81e3868c003c563c7dfccb989e7
Michael Long <mlong@clientresourcesinc.com>
2019-07-25 13:48:55 -0500

    Add Initial Demo Project

---
+++ RxSwiftWidgetsDemo/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  RxSwiftWidgetsDemo
+//
+//  Created by Michael Long on 7/25/19.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
--- Sources/RxSwiftWidgets/RxSwiftWidgets.swift
+++ Sources/RxSwiftWidgets/RxSwiftWidgets.swift
@@ -1,3 +1,7 @@
-struct RxSwiftWidgets {
-    var text = "Hello, World!"
+public struct RxSwiftWidgets {
+    public var text = "Hello, World!"
+    public init() {}
+    public func test() {
+        
+    }
 }
