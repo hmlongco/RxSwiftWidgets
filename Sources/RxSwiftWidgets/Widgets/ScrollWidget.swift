62380a95ba4b6f32d34f03f9ea273d36ed046f07
Michael Long <mlong@clientresourcesinc.com>
2019-08-06 08:20:12 -0500

    Update scrollview code

--- Sources/RxSwiftWidgets/Widgets/ScrollWidget.swift
+++ Sources/RxSwiftWidgets/Widgets/ScrollWidget.swift
@@ -10,6 +10,12 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+public enum WidgetScrollAxis {
+    case both
+    case vertical
+    case horizontal
+}
+
 public struct ScrollWidget
     : WidgetViewModifying
     , WidgetContaining
@@ -21,28 +27,70 @@ public struct ScrollWidget
     public var widget: Widget
     public var modifiers: WidgetModifiers?
     public var padding: UIEdgeInsets?
+    public var axis = WidgetScrollAxis.vertical
 
     public init(_ widget: Widget) {
         self.widget = widget
     }
 
     public func build(with context: WidgetContext) -> UIView {
-        let subview = widget.build(with: context)
 
-        let view = UIScrollView()
+        let contentView = widget.build(with: context)
+        let padding = self.padding ?? UIEdgeInsets.zero
+
+        let view = WidgetScrollView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .clear
-        view.addSubview(subview)
+        view.axis = axis
+        view.padding = padding
+        view.addSubview(contentView)
 
-        subview.widget.applyConstraints(padding: padding)
+        contentView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding.top).isActive = true
+        contentView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: padding.left).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding.bottom).isActive = true
+        contentView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -padding.right).isActive = true
 
         modifiers?.apply(to: view, with: context)
         
         return view
     }
 
+    public func axis(_ axis: WidgetScrollAxis) -> Self {
+        return modified { $0.axis = axis }
+    }
+
+    /// Pass an UIScrollViewDelegate object and it will be assigned as the delegate for the generated UIScrollView.
+    public func delegate(_ delegate: UIScrollViewDelegate) -> Self {
+        return modified(WidgetModifierBlock<WidgetScrollView> { view, _ in
+            view.delegateReference = delegate
+            view.delegate = delegate
+        })
+    }
+
     public func with(_ block: @escaping WidgetModifierBlockType<UIScrollView>) -> Self {
         return modified(WidgetModifierBlock(block))
+    }
+
+}
+
+internal class WidgetScrollView: UIScrollView, WidgetViewCustomConstraints {
+
+    var axis: WidgetScrollAxis = .both
+    var delegateReference: UIScrollViewDelegate?
+    var padding: UIEdgeInsets!
+
+    func addCustomConstraints() {
+        guard let superview = superview, let subview = subviews.first else { return }
+        switch axis {
+        case .vertical:
+            let padding = self.padding.left + self.padding.right
+            subview.widthAnchor.constraint(equalTo: superview.widthAnchor, constant: -padding).isActive = true
+        case .horizontal:
+            let padding = self.padding.top + self.padding.bottom
+            subview.heightAnchor.constraint(equalTo: superview.heightAnchor, constant: -padding).isActive = true
+        case .both:
+            break
+        }
     }
 
 }
