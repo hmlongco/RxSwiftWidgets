e8cd9ab8891835a1c2360ce064b01f74003f2813
Michael Long <mlong@clientresourcesinc.com>
2019-08-09 16:00:59 -0500

    Update label initializers, placeholder

--- Sources/RxSwiftWidgets/Widgets/ImageWidget.swift
+++ Sources/RxSwiftWidgets/Widgets/ImageWidget.swift
@@ -15,17 +15,28 @@ public struct ImageWidget: WidgetViewModifying
 
     public var debugDescription: String { "ImageWidget()" }
 
+    public var imageModifier: AnyWidgetModifier?
     public var modifiers: WidgetModifiers?
 
-    public var image: UIImage?
-
     public init(_ image: UIImage) {
-        self.image = image
+        self.imageModifier = WidgetModifier(keyPath: \UIImageView.image, value: image)
     }
 
-    public init(named name: String? = nil) {
-        if let name = name {
-            self.image = UIImage(named: name)
+    public init(named name: String) {
+        if let image = UIImage(named: name) {
+            self.imageModifier = WidgetModifier(keyPath: \UIImageView.image, value: image)
+        }
+    }
+
+    public init<O:ObservableElement>(_ observable: O) where O.Element == UIImage? {
+        self.imageModifier = WidgetModifierBlock<UIImageView> { view, context in
+            observable.asObservable().bind(to: view.rx.image).disposed(by: context.disposeBag)
+        }
+    }
+
+    public init<O:ObservableElement>(_ observable: O) where O.Element == String {
+        self.imageModifier = WidgetModifierBlock<UIImageView> { view, context in
+            observable.asObservable().map { UIImage(named: $0) }.bind(to: view.rx.image).disposed(by: context.disposeBag)
         }
     }
 
@@ -34,26 +45,17 @@ public struct ImageWidget: WidgetViewModifying
         let context = context.set(view: view)
         
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true // fixes transition animation bug with full screen background images
 
-        if let image = image {
-            view.image = image
-        }
+        imageModifier?.apply(to: view, with: context)
 
         modifiers?.apply(to: view, with: context)
         
         return view
     }
 
-    public func image<O:ObservableElement>(_ observable: O) -> Self where O.Element == UIImage? {
-        return modified(WidgetModifierBlock<UIImageView> { view, context in
-            observable.asObservable().bind(to: view.rx.image).disposed(by: context.disposeBag)
-        })
-    }
-
-    public func image<O:ObservableElement>(_ observable: O) -> Self where O.Element == String {
-        return modified(WidgetModifierBlock<UIImageView> { view, context in
-            observable.asObservable().map { UIImage(named: $0) }.bind(to: view.rx.image).disposed(by: context.disposeBag)
-        })
+    public func placeholder(_ image: UIImage) -> Self {
+        return modified(WidgetModifier(keyPath: \UIImageView.image, value: image))
     }
 
     public func with(_ block: @escaping WidgetModifierBlockType<UIImageView>) -> Self {
