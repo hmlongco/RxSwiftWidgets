b42ea09e03986469b1c0c3268e1985489bca43e9
Michael Long <mlong@clientresourcesinc.com>
2019-09-09 10:29:27 -0500

    On forward events on bindable elements.

--- Sources/RxSwiftWidgets/Rx/BindableElement.swift
+++ Sources/RxSwiftWidgets/Rx/BindableElement.swift
@@ -26,18 +26,24 @@ extension BehaviorRelay: BindableElement {
 
 extension BehaviorSubject: BindableElement {
     public func observe(_ observable: Observable<Element>) -> Disposable {
-        observable.bind(to: self)
+        observable.subscribe(onNext: { [weak self] (element) in
+            self?.onNext(element) // only forward onNext events
+        })
     }
 }
 
 extension PublishRelay: BindableElement {
     public func observe(_ observable: Observable<Element>) -> Disposable {
-        observable.bind(to: self)
+        observable.subscribe(onNext: { [weak self] (element) in
+            self?.accept(element) // only forward onNext events
+        })
     }
 }
 
 extension PublishSubject: BindableElement {
     public func observe(_ observable: Observable<Element>) -> Disposable {
-        observable.bind(to: self)
+        observable.subscribe(onNext: { [weak self] (element) in
+            self?.onNext(element) // only forward onNext events
+        })
     }
 }
