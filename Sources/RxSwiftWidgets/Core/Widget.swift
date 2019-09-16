0cc94129b4b7d0a51970514cbfc4a4bb4e850562
Michael Long <mlong@clientresourcesinc.com>
2019-09-16 07:39:46 -0500

    Core definitions for WidgetViewType and WidgetControllerType

--- Sources/RxSwiftWidgets/Core/Widget.swift
+++ Sources/RxSwiftWidgets/Core/Widget.swift
@@ -8,27 +8,48 @@
 
 import UIKit
 
-/// Core definition of a widget is an entity capable of building a specific UIView when asked.
-public protocol Widget {
+/// Base type for any type of widget.
+public protocol AnyWidget { }
+
+/// A special-purpose widget capable of building a specific UIViewController when asked.
+public protocol WidgetControllerType: AnyWidget {
+    /// Builds or returns a UIViewController that wraps the current widget.
+    func controller(with context: WidgetContext) -> UIViewController
+}
+
+/// Core definition of a widget as an entity capable of building a specific UIView when asked.
+public protocol WidgetViewType: WidgetControllerType {
+    /// Builds or returns a UIView that represents the current widget.
     func build(with context: WidgetContext) -> UIView
 }
 
-/// Namespace for many RxSwiftWidget Enumerations and Definitions
-public struct Widgets {
+/// Convenience type as most widgets are WidgetViewTypes.
+public typealias Widget = WidgetViewType
 
-}
+/// Namespace for many RxSwiftWidget Enumerations and Definitions
+public struct Widgets { }
 
 /// A widget that contains or wraps another widget
 public protocol WidgetContaining: Widget {
+    /// Variable containing the wrapped widget
     var widget: Widget { get }
 }
 
 /// A widget that holds a list of widgets, usually some form of stack
 public protocol WidgetsContaining: Widget {
+    /// Variable containing an array of the wrapped widgets
     var widgets: [Widget] { get }
 }
 
-extension Widget {
+/// Common functions on WidgetViewType.
+extension WidgetViewType {
+
+    /// Allows any widget to be pushed or presented on the navigation stack.
+    public func controller(with context: WidgetContext) -> UIViewController {
+        return UIWidgetHostController(self, with: context)
+    }
+
+    /// Walks widget tree of WidgetContaining and WidgetsContaining and calls closure on each node.
     public func walk(_ process: (_ widget: Widget) -> Void ) {
         func each(_ widget: Widget) {
             process(widget)
@@ -40,5 +61,6 @@ extension Widget {
         }
         each(self)
     }
+
 }
 
